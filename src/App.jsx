26e9e17c4776bebad1b1966e5fea1c440d29f3ec
ksenia-b <ksenia.barshchyk@gmail.com>
2024-08-30 14:40:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import './App.css'
+import Table from "./Table.tsx";
+
+function App() {
+  const [M, setM] = useState < number > (0);
+  const [N, setN] = useState < number > (0);
+
+  const handleGenerate = () => {
+
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          type="number"
+          value={M}
+          onChange={(e) => setM(parseInt(e.target.value, 10))}
+          placeholder="Rows"
+        />
+        <input
+          type="number"
+          value={N}
+          onChange={(e) => setN(parseInt(e.target.value, 10))}
+          placeholder="Columns"
+        />
+        <button onClick={handleGenerate}>Generate Matrix</button>
+        <Table matrix={matrix} />
+      </div>
+
+      <Table />
+    </>
+  )
+}
+
+export default App
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type CellId = number;
+export type CellValue = number;
+
+export type Cell = {
+    id: CellId;
+    amount: CellValue;
+};
